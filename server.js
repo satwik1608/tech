521974af7d7969747bc7696fc6776989f4c47180
satwik1608 <satwikkashyap1608@gmail.com>
2023-01-19 22:41:16 +0530

    fds

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
+const origin = req.headers.origin;
+
+res.setHeader("Access-Control-Allow-Origin", origin || "*");
+res.setHeader(
+  "Access-Control-Allow-Methods",
+  "POST, GET, PUT, DELETE, OPTIONS, XMODIFY"
+);
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "86400");
+res.setHeader(
+  "Access-Control-Allow-Headers",
+  "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+);
 app.use(express.json());
 app.use(bodyParser.json());
 
