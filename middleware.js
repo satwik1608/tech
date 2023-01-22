ea3d35eca40dee0d27493a453c4fe35a2ce832cd
satwik1608 <satwikkashyap1608@gmail.com>
2023-01-22 20:36:04 +0530

    fds

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ module.exports = {
 function cors(req, res, next) {
   const origin = req.headers.origin;
 
-  res.setHeader("Access-Control-Allow-Origin", privateKey);
+  res.setHeader("Access-Control-Allow-Origin", origin || "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "POST, GET, PUT, DELETE, OPTIONS, XMODIFY"
@@ -14,7 +14,7 @@ function cors(req, res, next) {
   res.setHeader("Access-Control-Max-Age", "86400");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,privatekey"
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
   );
 
   next();
