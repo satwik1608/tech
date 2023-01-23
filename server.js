fe4125f22c6407bcfc05b57df017e5f77aa19757
satwik1608 <satwikkashyap1608@gmail.com>
2023-01-23 11:57:26 +0530

    r

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
 app.use(middleware.cors);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
